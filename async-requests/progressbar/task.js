fb166a504801a506a6689ba997dcc1a0aa0084cd
dmiweb <dimbons@mail.ru>
2024-02-26 16:17:03 +0300

    synchronization PC

--- async-requests/progressbar/task.js
+++ async-requests/progressbar/task.js
@@ -1,20 +1,82 @@
 const form = document.getElementById('form');
-const file = document.querySelector('input[type="file"]');
 const progress = document.getElementById('progress');
 progress.value = 0;
 
 
-const formData = new FormData(form);
 
-const xhr = new XMLHttpRequest();
-xhr.open('POST', form.action);
+form.addEventListener("submit",
+  function (e) {
+    const formData = new FormData(form);
 
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  xhr.send(formData);
-});
+    const xhr = new XMLHttpRequest();
+    
+    xhr.open("POST", form.action, true);
+    xhr.onload = function (e) {
+      if (xhr.status == 200) {
+        
+      } else {
+        console.log(e.loaded);
+        console.log('error');
+        console.log(xhr.status);
+      }
+    };
 
-xhr.upload.addEventListener('progress', (e) => {
-  progress.value = (e.loaded / e.total) * 100;
-console.log(e.loaded);
-});
+    xhr.send(formData);
+    e.preventDefault();
+  },
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// form.addEventListener('submit', (e) => {
+//   e.preventDefault();
+
+//   const xhr = new XMLHttpRequest();
+
+//   xhr.upload.addEventListener('progress', (e) => {
+
+
+//     if (xhr.status === 200) {
+//       console.log('ok');
+//     } else {
+//       console.log('error');
+// console.log(xhr.status);
+//     }
+
+//     // progress.value = (e.loaded / e.total) * 100;
+//     // if (xhr.readyState === xhr.DONE) {
+//     //   alert('Загружено');
+//     // }
+//   });
+
+//   xhr.open('POST', form.action);
+//   const formData = new FormData(form);
+//   xhr.send(formData);
+// });
+
+
+
+
+
+
+// const formData = new FormData(form);
+
+// xhr.open('POST', form.action);
+// xhr.send(formData);
+
+
+// xhr.upload.addEventListener('progress', (e) => {
+//   progress.value = (e.loaded / e.total) * 100;
+// console.log(e.loaded);
+// });
